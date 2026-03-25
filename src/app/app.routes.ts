925fb42e59655e132dca970b92f7c13cb78c41d1
slaur88 <laurentsteve8@gmail.com>
2026-03-25 14:41:39 +0100

    navbar + errors + routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/page/home/home';
+import { Connection } from './features/page/connection/connection';
+import { Evenement } from './features/page/evenement/evenement';
+import { Profile } from './features/page/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path:'home',
+    component:Home
+},
+
+{
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+},
+
+{
+    path:"connection",
+    component: Connection
+},
+
+{
+    path:"evenement",
+    component: Evenement
+},
+
+{
+    path:'profile',
+    component: Profile
+},
+
+{
+    path:'**',
+    loadComponent: () => import("./features/errors/not-found/not-found")
+        .then(c => c.NotFound)
+}
+
+];
