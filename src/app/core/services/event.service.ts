33232ed2dbe8bdd30ff3975755c126b0af54abc7
f.russo <f.russo@GOS-VDI307>
2026-04-04 08:46:08 +0200

    Petits changements d'architecture

--- src/app/core/services/event.service.ts
+++ src/app/core/services/event.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { EventModel,EventModelDTO } from '../../shared/Models/event.model';
+import { EventModel,EventModelDTO } from '../../shared/models/event.model';
 
 @Injectable({providedIn: 'root',})
 
 export class EventService { 
 
   private readonly _httpClient:HttpClient = inject(HttpClient);
-  private readonly base_url ="http://localhost:4200/events";
+  private readonly base_url ="https://localhost:7262/api/event";
 
   getAllEvent(): Observable<EventModelDTO[]> {
     return this._httpClient.get<EventModelDTO[]>(this.base_url);
@@ -23,6 +23,6 @@ export class EventService {
     return this._httpClient.post<void>(this.base_url, event);
   }
   modifyEvent(id:string,event:Event): Observable <void>{
-    return this._httpClient.put<void>(this.base_url+"/"+id,event);
+    return this._httpClient.patch<void>(this.base_url+"/"+id,event);
   }
 }
